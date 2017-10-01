bb11ca80c7abd4233a88b3332ed45bbb8b93ac6a
huseyin.kilic <huseyin.kilic@amadeus.com>
2017-10-01 18:36:59 +0300

    spring boot project initialization

---
+++ src/main/java/com/hkilic/Swe573Application.java
@@ -0,0 +1,12 @@
+package com.hkilic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swe573Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Swe573Application.class, args);
+	}
+}
---
+++ src/test/java/com/hkilic/Swe573ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hkilic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Swe573ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
